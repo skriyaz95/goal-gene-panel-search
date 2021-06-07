8300aa0b18c084355a6f77de41e90cc8b5b1fa1e
cmonnom <guillaume.jim@gmail.com>
2021-06-07 15:03:56 -0500

    Update vuetify.ts
    Playing with themes

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,19 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: colors.cyan.darken1,
+            secondary: colors.grey.darken1,
+          },
+        //   dark: {
+        //     primary: colors.blue.lighten3,
+        //   },
+        },
+      },
 });
