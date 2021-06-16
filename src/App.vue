20926b947add1da98f26b6ecccbc083e1217e8f9
cmonnom <guillaume.jim@gmail.com>
2021-06-16 09:31:20 -0500

    Moved to nav component
    Refactored App.vue to use a navigation component. Moved labels to en.json

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,6 @@
 <template>
   <v-app :style="getBackgroundStyle()">
-    <v-navigation-drawer
-      expand-on-hover
-      bottom
-      clipped
-      app
-      :mini-variant.sync="mini"
-    >
-      <v-card v-show="mini" flat min-height="256" >
-        <v-img src="./assets/gti/gti_logo_nolabel_128.png"
-               alt="GTI Logo" width="80%" 
-               class="mt-2"
-               style="margin:auto"></v-img>
-        <v-img src="./assets/gti/gti_acronym_orig.png"
-               alt="GTI Logo" 
-               class="ml-2 mr-2 mt-10"
-               width="35px" min-width="35px" max-width="35px"
-        ></v-img>  
-      </v-card>
-      <v-card v-show="!mini" flat min-height="256" >  
-        <v-img src="./assets/gti/gti_logo_256.png"
-               alt="GTI Logo" width="70%" 
-               class="mt-2 mb-4"
-               style="margin:auto"></v-img>     
-        <v-img src="./assets/gti/gti_label_512.png"
-               alt="GTI Title"
-               class="ma-2"
-               width="225px" min-width="225px" max-width="225px"
-        ></v-img>
-      </v-card>
-      <v-divider></v-divider>
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item link :to="{name: 'Home'}" active-class="primary" exact>
-          <v-list-item-icon>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Home</v-list-item-title>
-        </v-list-item>
-        <v-list-item link :to="{name: 'Utils'}" active-class="primary" exact>
-          <v-list-item-icon>
-            <v-icon>mdi-cog</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Utils</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <navigation-menu/>
     <v-app-bar app color="primary" dark flat clipped-left>
       <div class="d-flex align-center">
         <span class="title">Find Genes in Panels</span>
@@ -81,15 +34,16 @@ import Vue from "vue";
 import axios from "axios";
 import { mapGetters } from "vuex";
 import { PanelPayload } from "@/types/panel-types";
+import NavigationMenu from "@/components/NavigationMenu.vue"
 
 export default Vue.extend({
   name: "App",
+  components: {NavigationMenu},
 
   data: () => ({
     sourceDir: "source_panels/",
     panelNames: new Array<string>(),
     publicPath: process.env.BASE_URL,
-    mini: true
   }),
   methods: {
     importExistingPanels(r: any) {
---
+++ src/components/NavigationMenu.vue
@@ -0,0 +1,71 @@
+/* eslint-disable vue/html-indent */
+<template>
+  <v-navigation-drawer
+    expand-on-hover
+    bottom
+    clipped
+    app
+    :mini-variant.sync="mini"
+  >
+    <v-card v-show="mini" flat min-height="256" >
+      <v-img src="../assets/gti/gti_logo_nolabel_128.png"
+             alt="GTI Logo" width="80%" 
+             class="mt-2"
+             style="margin:auto"></v-img>
+      <v-img src="../assets/gti/gti_acronym_orig.png"
+             alt="GTI Logo" 
+             class="ml-2 mr-2 mt-10"
+             width="35px" min-width="35px" max-width="35px"
+      ></v-img>  
+    </v-card>
+    <v-card v-show="!mini" flat min-height="256" >  
+      <v-img src="../assets/gti/gti_logo_256.png"
+             alt="GTI Logo" width="70%" 
+             class="mt-2 mb-4"
+             style="margin:auto"></v-img>     
+      <v-img src="../assets/gti/gti_label_512.png"
+             alt="GTI Title"
+             class="ma-2"
+             width="225px" min-width="225px" max-width="225px"
+      ></v-img>
+    </v-card>
+    <v-divider></v-divider>
+    <v-list
+      nav
+      dense
+    >
+      <v-list-item link :to="{name: 'Home'}" active-class="primary lighten-2" exact>
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>{{ $t("navigation.home.text") }}</v-list-item-title>
+      </v-list-item>
+      <v-list-item link :to="{name: 'Utils'}" active-class="primary lighten-2" exact>
+        <v-list-item-icon>
+          <v-icon>mdi-cog</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>{{ $t("navigation.utils.text") }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+          
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "NavigationMenu",
+  props: {
+  },
+  data: () => ({
+        mini: true
+  }),
+  methods: {
+  },
+  computed: {
+  },
+  mounted() {
+  },
+});
+</script>
