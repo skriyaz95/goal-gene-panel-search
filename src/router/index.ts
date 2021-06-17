d1e32d4fc2394b3b5219a1aa3efc801b112d0733
cmonnom <guillaume.jim@gmail.com>
2021-06-17 10:15:07 -0500

    Set title from route
    Set title from route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import i18n from '@/i18n'
 
 Vue.use(VueRouter)
 
@@ -9,7 +10,7 @@ const routes = [
     path: '/',
     name: 'Home',
     meta: {i18n: 'navigation.home'}, //useful to retrieve locale text in en.json for example
-
+    
     component: Home
   },
   {
@@ -24,5 +25,11 @@ const router = new VueRouter({
   routes: routes,
   mode: 'history'
 })
+
+router.beforeEach((to, from, next) => {
+  console.log();
+  document.title = "GTI " + (i18n.t(to.meta.i18n +  ".title.text") || '');
+  next();
+});
 
 export default router
