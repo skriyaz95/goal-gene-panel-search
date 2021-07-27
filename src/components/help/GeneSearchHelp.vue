4ada211ccefee5712089dc5f036cc4b98281591d
cmonnom <guillaume.jim@gmail.com>
2021-07-27 08:04:51 -0500

    Update GeneSearchHelp.vue

--- src/components/help/GeneSearchHelp.vue
+++ src/components/help/GeneSearchHelp.vue
@@ -10,8 +10,8 @@ or a help card or other tutorial content
       {{ $t('help.geneSearch.part1a') }}
       {{ $t('help.geneSearch.part1b') }}
     </div>
-    <div>{{ $t('help.geneSearch.part2a') }}</div>
     <div>
+      {{ $t('help.geneSearch.part2a') }}
       {{ $t('help.geneSearch.part2b') }}
       <b>{{ $t('parsedInput.title.text') }}</b>
       {{ $t('help.geneSearch.part2c') }}
